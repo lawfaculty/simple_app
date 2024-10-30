6a1e09882000eb8cbab0f2c27043a0dc63155fe0
lastcode_avocado_law <115080294+lawfaculty@users.noreply.github.com>
2024-10-30 17:40:32 +0300

    Update pyrebase.py

--- pyrebase/pyrebase.py
+++ pyrebase/pyrebase.py
@@ -2,7 +2,6 @@ import requests
 from requests import Session
 from requests.adapters import HTTPAdapter
 from requests.exceptions import HTTPError
-from urllib3.contrib.appengine import is_appengine_sandbox
 
 from urllib.parse import urlencode, quote
 import json
@@ -15,7 +14,6 @@ import threading
 import socket
 from oauth2client.service_account import ServiceAccountCredentials
 from gcloud import storage
-from requests_toolbelt.adapters import appengine
 from uuid import uuid4
 
 def initialize_app(config):
@@ -42,13 +40,7 @@ class Firebase:
                 self.credentials = ServiceAccountCredentials.from_json_keyfile_name(config["serviceAccount"], scopes)
             if service_account_type is dict:
                 self.credentials = ServiceAccountCredentials.from_json_keyfile_dict(config["serviceAccount"], scopes)
-        if is_appengine_sandbox():
-            # Fix error in standard GAE environment
-            # is releated to https://github.com/kennethreitz/requests/issues/3187
-            # ProtocolError('Connection aborted.', error(13, 'Permission denied'))
-            adapter = appengine.AppEngineAdapter(max_retries=3)
-        else:
-            adapter = HTTPAdapter()
+        adapter = HTTPAdapter()
 
         for scheme in ('http://', 'https://'):
             self.requests.mount(scheme, adapter)
